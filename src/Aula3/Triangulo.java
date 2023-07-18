d370d2c2e66d8990b73f64c6c55693ffb8cf561d
Pedro Henrique <phenrique.guerra@hotmail.com>
2023-07-17 21:32:56 -0300

    Resolução de exercícios Aula3

--- src/Aula3/Triangulo.java
+++ src/Aula3/Triangulo.java
@@ -3,12 +3,30 @@ package Aula3;
 public class Triangulo {
 
     public static void main(String[] args) {
-        double area = calculaArea(9, 7, 14);
-        System.out.printf("A área do triangulo é de: %.2f", area);
+        double area = calculaArea(10, 15, 0);
+        if(area > 0){
+            System.out.printf("A área do triangulo é de: %.2f", area);
+
+        }
+        else{
+            System.out.println("Os lados informados não formam um triangulo");
+        }
+
     }
     public static double calculaArea(int a, int b, int c){
-        int p = (a + b + c) / 2;
-        double area =  (Math.sqrt(p * (p - a) * (p - b) * (p - c)));
-        return area;
+        boolean triangulo = verificaTriangulo(a, b, c);
+        if(triangulo) {
+            int p = (a + b + c) / 2;
+            double area =  (Math.sqrt(p * (p - a) * (p - b) * (p - c)));
+            return area;
+        }
+        else return 0;
+    }
+
+    public static boolean verificaTriangulo(int a, int b, int c){
+        if (a < b + c || b < a + c || c < a + b){
+            return true;
+        }
+        else return false;
     }
 }
